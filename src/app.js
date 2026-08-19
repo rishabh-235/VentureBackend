caedd1f19c7f736b78edafca11204e7b0fc5d3e4
rishabh <rishabhguptalahar@gmail.com>
2026-08-20 00:25:42 +0530

    fix index.js bug

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
